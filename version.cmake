2b2721a54216445d5c7079d406abc8c96d24501e
Akito-dataminer <akito.dataminer@gmail.com>
2022-10-25 12:49:12 +0900

    increment - build number

--- version.cmake
+++ version.cmake
@@ -1,7 +1,7 @@
 set(PRODUCT_NAME "")
 set(VERSION_MAJOR 0)
 set(VERSION_MINOR 0)
-set(BUILD_NUMBER 0)
+set(BUILD_NUMBER 1)
 set(LICENSE_HOLDER "Akito Matsumoto")
 
 configure_file(version.h.in include/version.h)
