ebaedac70335c15aec9d6911f8864907452d0e11
Akito-dataminer <akito.dataminer@gmail.com>
2022-11-18 14:10:26 +0900

    remove - include/version.h from git

--- include/version.h
+++
@@ -1,7 +0,0 @@
-#define VERSION_MAJOR   0
-#define VERSION_MINOR   0
-#define BUILD_NUMBER    0
- 
-#define VERSION_STRING  \
-    " 0.0.0\n" \
-    "Copyright(C) 2022 Akito Matsumoto all rights reserved."
